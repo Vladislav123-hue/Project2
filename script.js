b9ffd159f48ec1b8230e9a4c88bca58cf9ec897d
Влад Кабалоев <gebirge1999@gmail.com>
2025-04-07 17:35:44 +0300

    add basic 4 operations (+,-,/,*)

--- script.js
+++ script.js
@@ -1,27 +1,103 @@
 const buttons = document.querySelectorAll(".btn");
 const operationButtons = document.querySelectorAll(".op-btn")
 const actionButtons = document.querySelectorAll(".ac-btn")
+let screenContent = document.getElementById("calculator-screen");
+let numberCollection = [];
+let createdNumber = "";
+let operationButtonsList = [];
+let hasBeenInUse = false;
+let finalResult = "";
+operationButtons.forEach(button => {
+    operationButtonsList.push(button.innerText);
+})
 buttons.forEach(button => {
     button.addEventListener("click", function () {
-        let value = button.innerText; // Получаем текст кнопки
-        handleInput(value); // Передаём значение в метод
+        let value = button.innerText; // Select the content of the button
+            console.log("Numeral recognized")
+            numberInput(value); // Sending in a method     
     });
 });
 
 operationButtons.forEach(button => {
     button.addEventListener("click", function () {
-        let value = button.innerText; // Получаем текст кнопки
-        handleInput(value); // Передаём значение в метод
+        operationSignSelected = true; // Operation sign selected 
+        let value = button.innerText;
+        console.log("Operation sign " + value + " recognized");
+        addNumbToCollection(value);
     });
 });
 
 actionButtons.forEach(button => {
     button.addEventListener("click", function () {
-        let value = button.innerText; // Получаем текст кнопки
-        handleInput(value); // Передаём значение в метод
+        let value = button.innerText;
+        addNumbToCollection(value);
     });
 });
 
-function handleInput(value) {
-    console.log(value, "кнопка нажата");
+
+
+function numberInput(value) {
+    console.log(value, " numeral pressed");
+    creatingNumber(value);
+}
+
+function operationSignInput(value) {
+    console.log(value, " operationSign pressed");
+}
+
+
+
+
+function creatingNumber (value) {
+createdNumber += value;
+screenContent.innerText += value;
+}
+
+
+function addNumbToCollection(value) {
+        
+    if (value != "=") 
+    {
+
+    
+        if (createdNumber != "") {
+            numberCollection.push(createdNumber);
+            console.log("number " + createdNumber + " added to the collection");
+        }
+        
+        if (operationButtonsList.includes(numberCollection[numberCollection.length - 1])) {
+            numberCollection.length = numberCollection.length - 1;
+            console.log("Operation sign repeated, list length " + numberCollection);
+        }
+        if (!operationButtonsList.includes(numberCollection[numberCollection.length - 1]))
+        {
+        numberCollection.push(value);
+        console.log("Operation sign " + value + " added to the collection");
+        createdNumber = "";
+        operationSignSelected = false;
+        screenContent.innerText = "";
+        console.log("number " + createdNumber + "removed from the screen");
+        console.log("list equal " + numberCollection);
+        }
+    }
+        if (value == "=") {
+            numberCollection.push(createdNumber);
+            console.log("list equal " + numberCollection);
+            calculate(numberCollection);
+        }
+        
+      
+}
+function calculate(numberCollection) {
+    let stringResult = "";
+    for (i = 0; i < numberCollection.length; i++) {
+        stringResult += numberCollection[i];
+    }
+    let result = Function("return " + stringResult)();
+console.log(result); 
+screenContent.innerText = result;
+createdNumber = result;
+numberCollection.length = 0;
+console.log("List cleaned");
+
 }
